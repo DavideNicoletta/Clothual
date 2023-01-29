9dc73e3da6dc6071a4cee8827e2c635e333de903
DanieleDituri01 <d.dituri@campus.unimib.it>
2023-01-29 20:15:39 +0100

    Prima prova Gmaps

--- app/src/main/java/com/example/clothual/UI/core/Map/MapFragment.java
+++ app/src/main/java/com/example/clothual/UI/core/Map/MapFragment.java
@@ -1,131 +1,40 @@
 package com.example.clothual.UI.core.Map;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
+import com.example.clothual.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import com.example.clothual.databinding.FragmentMapBinding;
+public class MapFragment extends AppCompatActivity implements OnMapReadyCallback {
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.views.CustomZoomButtonsController;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-@SuppressWarnings("deprecation")
-public class MapFragment extends Fragment {
-
-    public FragmentMapBinding binding;
-    private MyLocationNewOverlay locationOverlay;
-    long pressedTime = 0;
-
-    public MapFragment() {
-
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance() {
-        return new MapFragment();
-    }
+    private GoogleMap mMap;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        binding = FragmentMapBinding.inflate(getLayoutInflater());
-        return binding.getRoot();
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                &&
-        ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ){
-
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-        }
-
-        Context ctx = getActivity().getApplicationContext();
-
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-
-
-
-        assert ctx != null;
-        GpsMyLocationProvider provider = new GpsMyLocationProvider(ctx);
-        provider.addLocationSource(LocationManager.NETWORK_PROVIDER);
-        locationOverlay = new MyLocationNewOverlay(provider, binding.mapView);
-        locationOverlay.enableFollowLocation();
-        locationOverlay.runOnFirstFix(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("MyTag", String.format("First location fix: %s", locationOverlay.getLastFix()));
-            }
-        });
-        binding.mapView.getOverlayManager().add(locationOverlay);
-        binding.mapView.setTileSource(TileSourceFactory.MAPNIK);
-        binding.mapView.setBuiltInZoomControls(true);
-        binding.mapView.setMultiTouchControls(true);
-        binding.mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
-        IMapController mapController = binding.mapView.getController();
-        mapController.setZoom(17);
-        binding.center.setOnClickListener(view1 -> {
-            if (pressedTime + 2000 > System.currentTimeMillis()) {
-                locationOverlay.enableFollowLocation();
-                mapController.setZoom(17);
-            } else {
-                locationOverlay.enableFollowLocation();
-                Toast.makeText(getActivity().getBaseContext(), "Premi due volte per lo zoom.", Toast.LENGTH_LONG).show();
-            }
-            pressedTime = System.currentTimeMillis();
-        });
-
-    }
-
-    public void onResume(){
-        super.onResume();
-        locationOverlay.enableMyLocation();
-    }
-
-    public void onPause(){
-        super.onPause();
-        locationOverlay.disableMyLocation();
-    }
-
 }
+
