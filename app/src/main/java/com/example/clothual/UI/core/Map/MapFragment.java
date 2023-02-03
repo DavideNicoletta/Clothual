71f25fd942cb7c7c8d0d57773e248a7b401214b9
DanieleDituri01 <d.dituri@campus.unimib.it>
2023-02-03 10:56:28 +0100

    prima prova gps

--- app/src/main/java/com/example/clothual/UI/core/Map/MapFragment.java
+++ app/src/main/java/com/example/clothual/UI/core/Map/MapFragment.java
@@ -1,5 +1,8 @@
 package com.example.clothual.UI.core.Map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,18 +10,20 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
 import com.example.clothual.R;
 import com.example.clothual.databinding.FragmentMapBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapFragment extends Fragment {
 
@@ -26,9 +31,11 @@ public class MapFragment extends Fragment {
 
     private FragmentMapBinding binding;
 
-    public MapFragment(){
+    public MapFragment() {
 
     }
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -38,8 +45,7 @@ public class MapFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
     @Nullable
@@ -50,19 +56,43 @@ public class MapFragment extends Fragment {
         return binding.getRoot();
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment supportMapFragment=(SupportMapFragment)
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.mapView);
         // Async map
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 // When map is loaded
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    // Logic to handle location object
+                                }
+                            }
+                        });
+
+                Location pos = fusedLocationClient.getLastLocation().getResult();
                 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(LatLng latLng) {
+                        latLng = new LatLng(pos.getLatitude(), pos.getLongitude());
                         // When clicked on map
                         // Initialize marker options
                         MarkerOptions markerOptions=new MarkerOptions();
@@ -80,11 +110,6 @@ public class MapFragment extends Fragment {
                 });
             }
         });
-     /*   SupportMapFragment mapFragment = (SupportMapFragment) getParentFragmentManager()
-                .findFragmentById(R.id.mapView);
-        mapFragment.getMapAsync(this);
-
-      */
     }
 
 }
